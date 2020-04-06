c92edaddaaa5b9b88403cedb0e25d60e6b133199
kairos03 <kairos9603@gmail.com>
2020-04-07 00:09:24 +0900

    add login page

---
+++ src/components/Login/LoginForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-form ref="loginForm">
+    <v-card-text>
+      <v-text-field
+        v-model="userName"
+        label="대화명"
+        required
+      ></v-text-field>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        color="success"
+        @click="joinSubmit"
+      >
+        Join
+      </v-btn>
+    </v-card-actions>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm',
+  props: ['join'],
+  data: () => ({
+    userName: ''
+  }),
+  methods: {
+    joinSubmit () {
+      this.$emit('join', this.userName)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container grid-list-sm fluid>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-card-title primary-title>
+            Login
+          </v-card-title>
+          <login-form :join="joinSubmit"></login-form>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import LoginForm from '@/components/Login/LoginForm'
+
+export default {
+  name: 'Login',
+  data: () => ({
+  }),
+  components: {
+    LoginForm
+  },
+  created () {
+  },
+  methods: {
+    joinSubmit (userName) {
+      this.$router.push(`/chat-room/${userName}`)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
